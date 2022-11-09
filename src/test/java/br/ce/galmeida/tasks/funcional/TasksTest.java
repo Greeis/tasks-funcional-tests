09af427706658180b9b296719b7b28c04a1e2201
Graziele Almeida <galmeida@viajanet.com.br>
2022-11-08 22:20:02 -0300

    Corrigindo alert

--- src/test/java/br/ce/galmeida/tasks/funcional/TasksTest.java
+++ src/test/java/br/ce/galmeida/tasks/funcional/TasksTest.java
@@ -40,7 +40,7 @@ public class TasksTest {
 			driver.findElement(By.id("saveButton")).click();
 			
 			String message = driver.findElement(By.id("message")).getText();
-			Assert.assertEquals("Sucess!", message);
+			Assert.assertEquals("Success!", message);
 		} finally {
 			driver.quit();
 		}	
@@ -110,12 +110,12 @@ public class TasksTest {
 			driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
 			driver.findElement(By.id("saveButton")).click();
 			String message = driver.findElement(By.id("message")).getText();
-			Assert.assertEquals("Sucess!", message);
+			Assert.assertEquals("Success!", message);
 			
 			
 			driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
 			message = driver.findElement(By.id("message")).getText();
-			Assert.assertEquals("Sucess!", message);
+			Assert.assertEquals("Success!", message);
 		} finally {
 			driver.quit();
 		}	
